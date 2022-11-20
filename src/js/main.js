200ffd869f1aa628ad0c23cd39a73678b5613c4b
Andrew Efremov <56955307+ka1tzyu@users.noreply.github.com>
2022-11-20 10:41:46 +0200

    fix: bg minifying

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,6 @@
-// Prevent transition loading from page load
-window.onload = () => {
-    document.querySelectorAll('.prevent-load-transition').forEach(e => {
-        e.classList.remove('prevent-load-transition')
-    });
-}
-
-// Declaring necessary DOM objects
+// Declare necessary DOM objects and manage effects toggle switch
 const effectBtn = document.getElementById('effects-toggle');
 const bgHtmlWrapper = document.getElementById('background-effects');
-
-effectBtn.addEventListener('click', switchBackground);
 
 if (window.localStorage.getItem('bgEffects') === null) {
     window.localStorage.setItem('bgEffects', 'false');
@@ -20,7 +11,7 @@ if (window.localStorage.getItem('bgEffects') === 'true') {
     effectBtn.classList.toggle('effects-toggle_active');
 }
 
-function switchBackground() {
+effectBtn.addEventListener('click', () => {
     bgHtmlWrapper.classList.toggle('background-effects_active');
     effectBtn.classList.toggle('effects-toggle_active');
     if (window.localStorage.getItem('bgEffects') === 'true') {
@@ -29,5 +20,17 @@ function switchBackground() {
     } else {
         effectBtn.setAttribute('title', 'Disable magic');
         window.localStorage.setItem('bgEffects', 'true');
+    }
+});
+
+
+// Prevent transition loading from page load and minify background for short pages
+window.onload = () => {
+    document.querySelectorAll('.prevent-load-transition').forEach(e => {
+        e.classList.remove('prevent-load-transition')
+    });
+
+    if (document.body.clientHeight < 1500) {
+        bgHtmlWrapper.classList.add('background-effects_minify');
     }
 }
